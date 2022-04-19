6cf7a2825102fe11c4647ad2a26b778601be71bc
AkolenDvdnk <ak0lenfs@gmail.com>
2022-04-19 12:40:04 +0300

    Character movement

---
+++ Ruby Adventure 2D Beginner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 moveInput;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        CheckInput();
+    }
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+    private void CheckInput()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+    }
+    private void ApplyMovement()
+    {
+        rb.velocity = moveInput * speed;
+    }
+}
